2b25a199b7bd9868a225a8327a91afa083bbb5f0
sandunrmst <sandunalugc@gmail.com>
2023-06-10 14:46:30 +0530

    Update File

--- SlideShow-3/script.js
+++ SlideShow-3/script.js
@@ -4,11 +4,11 @@
 
             let next = document.getElementById('next');
             let previous = document.getElementById('previous');
-            let container = document.getElementById('content');
+            let container = document.getElementById('imageContainer');
             
-            imageList = ["../SlideShow/image1.jpg","../SlideShow/image2.jpg","../SlideShow/image3.jpg", "../SlideShow/image4.jpg","../SlideShow/image5.jpg"];
+            let imageList = ["../SlideShow/image1.jpg","../SlideShow/image2.jpg","../SlideShow/image3.jpg", "../SlideShow/image4.jpg","../SlideShow/image5.jpg"];
 
-            currentImage =0;
+            let currentImage =0;
 
             next.addEventListener('click', function(event){
                 event.preventDefault(); // This privent when user click on anchor tag it will navigate to linked address.
@@ -22,7 +22,12 @@
                 newSlideImage.src = imageList[currentImage];
                 newSlideImage.className = 'fadeinimg';
                 container.appendChild(newSlideImage);
-
+                
+                
+                if (container.children.length > 2){
+                    container.removeChild(container.children[0]); //Here remove additional append img tags from document
+                }
+                
             });
 
             previous.addEventListener('click', function(){
@@ -34,10 +39,11 @@
                 newSlideImage.src = imageList[currentImage];
                 newSlideImage.className = 'fadeinimg';
                 container.appendChild(newSlideImage);
+
+                if (container.children.length > 2){
+                    container.removeChild(container.children[0]); //Here remove additional append img tags from document
+                }
             });
-
-
-        
 
         }());
 
